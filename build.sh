c01cb23658637535490b98a2c61db8ba698994d7
Andrew Chan <andrewchan@improbable.io>
2019-01-21 16:54:51 +0000

    Removed c# generation from loop

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")"
 
 WORKER_DIRS=(HelloWorker OtherWorkers/DiceWorker OtherWorkers/Interactive/client)
 BUILD_PLATFORMS=(macOS64 Windows64 Linux64)
-DOWNLOAD_DIR="$(pwd)/build"
+DOWNLOAD_DIR="$(pwd)/packages"
 BUILD_DIR="$(pwd)"
 SDK_VERSION="13.5.1"
 mkdir -p "${DOWNLOAD_DIR}"
@@ -42,15 +42,14 @@ if isWindows; then
   BUILD_TOOL="MSBuild.exe"
 fi
 
-
 retrievePackage() {
   TYPE=$1
   PACKAGE=$2
+  TARGETDIR=$3
 
   pushd "${DOWNLOAD_DIR}"
-  if [ ! -f "${PACKAGE}.zip" ]; then
-    spatial package retrieve --force "${TYPE}" "${PACKAGE}" "${SDK_VERSION}" "${PACKAGE}.zip"
-    unzip -o "${PACKAGE}.zip"
+  if [ ! -d "${TARGETDIR}" ]; then
+    spatial package get --force --unzip "${TYPE}" "${PACKAGE}" "${SDK_VERSION}" "${TARGETDIR}"
   fi
   popd
 }
@@ -58,36 +57,38 @@ retrievePackage() {
 # Get the tools:
 # * Spatial Schema compiler
 # * Standard Library Schemas
-# * Core SDK for all platforms to enable building workers for MacOS, Windows or Linux
 # * C# SDK
-retrievePackage "tools" "schema_compiler-x86_64-${PLATFORM_NAME}"
-retrievePackage "schema" "standard_library"
-retrievePackage "worker_sdk" "csharp"
-retrievePackage "worker_sdk" "core-dynamic-x86_64-win32"
-retrievePackage "worker_sdk" "core-dynamic-x86_64-linux"
-retrievePackage "worker_sdk" "core-dynamic-x86_64-macos"
+# * Core SDK for all platforms to enable building workers for MacOS, Windows or Linux
+retrievePackage "tools" "schema_compiler-x86_64-${PLATFORM_NAME}" "schema_compiler"
+retrievePackage "schema" "standard_library" "standard_library"
+retrievePackage "worker_sdk" "csharp" "lib/csharp"
+retrievePackage "worker_sdk" "core-dynamic-x86_64-win32" "lib/win64"
+retrievePackage "worker_sdk" "core-dynamic-x86_64-linux" "lib/linux64"
+retrievePackage "worker_sdk" "core-dynamic-x86_64-macos" "lib/macos64"
+
+# Generate C# code from the schemas:
+mkdir -p "${DOWNLOAD_DIR}"/csharp
+"${DOWNLOAD_DIR}"/schema_compiler/schema_compiler \
+  --schema_path="${BUILD_DIR}"/SpatialOS/schema \
+  --schema_path="$DOWNLOAD_DIR"/standard_library \
+  --csharp_out="${DOWNLOAD_DIR}"/csharp \
+  --load_all_schema_on_schema_path \
+  "${BUILD_DIR}"/SpatialOS/schema/*.schema \
+  "${DOWNLOAD_DIR}"/standard_library/improbable/*.schema
 
 # For each worker:
 for WORKER in "${WORKER_DIRS[@]}"; do
-  pushd "${BUILD_DIR}/${WORKER}/src"
-  # Compile the schema
-  OUT_DIR=improbable/generated
-  mkdir -p "$OUT_DIR"
-  "$DOWNLOAD_DIR"/schema_compiler \
-    --schema_path="${BUILD_DIR}/SpatialOS/schema" \
-    --schema_path="$DOWNLOAD_DIR" \
-    --csharp_out="$OUT_DIR" \
-    --load_all_schema_on_schema_path \
-    ${BUILD_DIR}/SpatialOS/schema/*.schema \
-    ${DOWNLOAD_DIR}/improbable/*.schema
-
-  # Compile UserCode+SDK+C#Schema into a binary
+  pushd "${BUILD_DIR}/${WORKER}"/src
+  # Compile UserCode + GeneratedC# + CoreSDK + C#SDK into a binary
+  mkdir -p improbable/generated
   mkdir -p improbable/dependencies/managed
   mkdir -p improbable/dependencies/native
-  cp "$DOWNLOAD_DIR"/Improbable.WorkerSdkCsharp.dll improbable/dependencies/managed
-  cp "$DOWNLOAD_DIR"/*.dylib improbable/dependencies/native
-  cp "$DOWNLOAD_DIR"/*.dll improbable/dependencies/native
-  cp "$DOWNLOAD_DIR"/*.so improbable/dependencies/native
+  cp -R "${DOWNLOAD_DIR}"/csharp/* improbable/generated
+  cp "${DOWNLOAD_DIR}"/lib/csharp/* improbable/dependencies/managed
+  cp "${DOWNLOAD_DIR}"/lib/win64/* improbable/dependencies/native
+  cp "${DOWNLOAD_DIR}"/lib/linux64/* improbable/dependencies/native
+  cp "${DOWNLOAD_DIR}"/lib/macos64/* improbable/dependencies/native
+
   for PLATFORM in "${BUILD_PLATFORMS[@]}"; do
     ${BUILD_TOOL} CsharpWorker.sln /property:Configuration=Release /property:Platform="$PLATFORM"
     cp -r bin ..
@@ -97,14 +98,14 @@ for WORKER in "${WORKER_DIRS[@]}"; do
   popd
 done
 
-# Compile the schemas into a schema descriptor
-mkdir -p "${BUILD_DIR}/SpatialOS/schema/bin"
-"${DOWNLOAD_DIR}/schema_compiler" \
-  --schema_path="${BUILD_DIR}/SpatialOS/schema" \
-  --schema_path="${DOWNLOAD_DIR}" \
+# Generate a schema descriptor from the schemas:
+mkdir -p "${BUILD_DIR}"/SpatialOS/schema/bin
+"${DOWNLOAD_DIR}"/schema_compiler/schema_compiler \
+  --schema_path="${BUILD_DIR}"/SpatialOS/schema \
+  --schema_path="${DOWNLOAD_DIR}"/standard_library \
   --load_all_schema_on_schema_path \
-  --descriptor_set_out="${BUILD_DIR}/SpatialOS/schema/bin/schema.descriptor" \
-  ${DOWNLOAD_DIR}/improbable/*.schema \
-  ${BUILD_DIR}/SpatialOS/schema/*.schema
+  --descriptor_set_out="${BUILD_DIR}"/SpatialOS/schema/bin/schema.descriptor \
+  "${DOWNLOAD_DIR}"/standard_library/improbable/*.schema \
+  "${BUILD_DIR}"/SpatialOS/schema/*.schema
 
 echo "Build complete"
