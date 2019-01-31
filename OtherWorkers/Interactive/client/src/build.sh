57404b1f3e70db06fe7e8e2acd50ac9887f9bf64
Sisi Liang <sisi@improbable.io>
2019-01-31 17:47:21 +0000

    removed MSBuild.exe

--- OtherWorkers/Interactive/client/src/build.sh
+++ OtherWorkers/Interactive/client/src/build.sh
@@ -4,9 +4,6 @@ set -e -x
 
 BUILD_PLATFORMS=(macOS64 Windows64 Linux64)
 BUILD_TOOL="msbuild"
-if isWindows; then
-  BUILD_TOOL="MSBuild.exe"
-fi
 
 echo "${BUILD_PLATFORMS}"
 for PLATFORM in "${BUILD_PLATFORMS[@]}"; do
