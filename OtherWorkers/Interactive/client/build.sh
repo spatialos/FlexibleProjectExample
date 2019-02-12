cf71d57b054bf6ecbc79504349d91c9b52dd7511
Sisi Liang <sisi@improbable.io>
2019-02-12 10:17:07 +0000

    added MSBuild.exe back

--- OtherWorkers/Interactive/client/build.sh
+++ OtherWorkers/Interactive/client/build.sh
@@ -3,7 +3,23 @@
 set -e -x
 
 BUILD_PLATFORMS=(macOS64 Windows64 Linux64)
+
+function isLinux() {
+  [[ "$(uname -s)" == "Linux" ]]
+}
+
+function isMacOS() {
+  [[ "$(uname -s)" == "Darwin" ]]
+}
+
+function isWindows() {
+  ! (isLinux || isMacOS)
+}
+
 BUILD_TOOL="msbuild"
+if isWindows; then
+  BUILD_TOOL="MSBuild.exe"
+fi
 
 echo "${BUILD_PLATFORMS}"
 for PLATFORM in "${BUILD_PLATFORMS[@]}"; do
